8bdb9ad368acbf446ed00623ce3562cf2918cb38
adam.bohannon <adam.bohannon@housecallpro.com>
2025-03-22 21:06:52 -0700

    Added icons

--- client/src/components/TodayModal.tsx
+++ client/src/components/TodayModal.tsx
@@ -1,4 +1,5 @@
 import { Modal, Typography, Space, Button } from 'antd';
+import { SmileOutlined, MehOutlined, FrownOutlined } from '@ant-design/icons';
 
 const { Title } = Typography;
 
@@ -10,20 +11,71 @@ interface TodayModalProps {
 const TodayModal = ({ isOpen, onClose }: TodayModalProps) => {
   return (
     <Modal
-      title={<Title level={4} style={{ margin: 0 }}>Mood Check-in</Title>}
       open={isOpen}
       onCancel={onClose}
       footer={null}
       centered
     >
       <Space direction="vertical" style={{ width: '100%', textAlign: 'center' }}>
-        <Typography.Text style={{ fontSize: '18px' }}>
-          How are you today?
-        </Typography.Text>
-        <Space style={{ marginTop: '20px' }}>
-          <Button type="primary" onClick={onClose}>
-            Submit
-          </Button>
+        <Title level={4}>
+          How are you?
+        </Title>
+
+        <Space size="small" style={{ justifyContent: 'center' }}>
+          <Space direction="vertical" align="center">
+            <Button
+              type="text"
+              style={{ fontSize: '24px', height: '60px', width: '60px' }}
+              onClick={onClose}
+            >
+              <SmileOutlined style={{ fontSize: '32px', color: '#52c41a' }} />
+            </Button>
+            <Typography.Text>Great</Typography.Text>
+          </Space>
+
+          <Space direction="vertical" align="center">
+            <Button
+              type="text"
+              style={{ fontSize: '24px', height: '60px', width: '60px' }}
+              onClick={onClose}
+            >
+              <SmileOutlined style={{ fontSize: '32px', color: '#91d5ff' }} />
+            </Button>
+            <Typography.Text>Good</Typography.Text>
+          </Space>
+
+          <Space direction="vertical" align="center">
+            <Button
+              type="text"
+              style={{ fontSize: '24px', height: '60px', width: '60px' }}
+              onClick={onClose}
+            >
+              <MehOutlined style={{ fontSize: '32px', color: '#faad14' }} />
+            </Button>
+            <Typography.Text>Meh</Typography.Text>
+          </Space>
+
+          <Space direction="vertical" align="center">
+            <Button
+              type="text"
+              style={{ fontSize: '24px', height: '60px', width: '60px' }}
+              onClick={onClose}
+            >
+              <FrownOutlined style={{ fontSize: '32px', color: '#fa8c16' }} />
+            </Button>
+            <Typography.Text>Bad</Typography.Text>
+          </Space>
+
+          <Space direction="vertical" align="center">
+            <Button
+              type="text"
+              style={{ fontSize: '24px', height: '60px', width: '60px' }}
+              onClick={onClose}
+            >
+              <FrownOutlined style={{ fontSize: '32px', color: '#f5222d' }} />
+            </Button>
+            <Typography.Text>Awful</Typography.Text>
+          </Space>
         </Space>
       </Space>
     </Modal>
